c465eba7afef7d6e6ad4323dbaae32de04debd7e
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-11-09 03:18:38 +0200

    adaptated header, added burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -25,3 +25,30 @@ const offersSlider = new Swiper('.offers__swiper', {
     disableOnInteraction: false,
   },
 })
+
+// бургер меню
+
+const burgerBtn  = document.querySelector('.burger__btn')
+const burgerMenu = document.querySelector('.menu')
+const body = document.body
+
+burgerBtn.addEventListener('click', burgerMenuHandler) 
+
+function burgerMenuHandler(e) {
+  burgerBtn.classList.toggle('active')
+  burgerMenu.classList.toggle('active')
+  body.classList.toggle('scrollLock')
+}
+
+function closeBurgerMenu(e) {
+  if (e.target !== burgerMenu) {
+    burgerMenu.classList.remove('active')
+  }
+}
+
+burgerMenu.addEventListener('click', e => {
+  if (e.target.classList.contains('menu__link')) {
+    burgerMenuHandler()
+    
+  }
+})
