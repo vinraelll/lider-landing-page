59b059886fec34a9991d2a0e2c52b686fe75e303
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-11-12 16:26:43 +0200

    finished mobile adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,3 @@
-// import Swiper from 'swiper'; 
-// не работает импорт модуля //
-
-const newsSlider = new Swiper('.news__swiper', {
-  slidesPerView: 3,
-  speed: 400,
-  spaceBetween: 30,
-  loop: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-})
-
 const offersSlider = new Swiper('.offers__swiper', {
   direction: 'vertical',
   slidesPerView: 3,
@@ -24,6 +9,34 @@ const offersSlider = new Swiper('.offers__swiper', {
     delay: 3000,
     disableOnInteraction: false,
   },
+})
+
+const newsSlider = new Swiper('.news__swiper', {
+  slidesPerView: 3,
+  speed: 400,
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    319: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    }
+  }
 })
 
 // бургер меню
